39f2469806384ab26ce5eb4340f874a925d68e92
Otávio Morais <otavio.morais@rarolabs.com.br>
2023-07-09 22:49:03 -0300

    feat: adding swagger to project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { expensesDataSource } from './database/expenses.datasource';
 import { ApplicationExceptionFilter } from './app.errors';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,28 @@ async function bootstrap() {
   if (process.env.NODE_ENV !== 'test' && !expensesDataSource.isInitialized) {
     expensesDataSource.initialize();
   }
+
+  const { version } = await import('../package.json');
+
+  const config = new DocumentBuilder()
+    .setTitle('Expenses API')
+    .setDescription('REST API to manage users expenses.')
+    .setVersion(version)
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'userAuth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
